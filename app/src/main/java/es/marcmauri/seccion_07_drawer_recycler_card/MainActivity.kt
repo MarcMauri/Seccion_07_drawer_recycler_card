66fb29aa6ab2ea349e6ed060a50fbc167ebf58ed
Marc from GitKraken <marcmauri92@gmail.com>
2018-09-17 12:40:54 +0200

    Burger icon added to open the Drawable + cool transparent style starting for API 21

--- app/src/main/java/es/marcmauri/seccion_07_drawer_recycler_card/MainActivity.kt
+++ app/src/main/java/es/marcmauri/seccion_07_drawer_recycler_card/MainActivity.kt
@@ -1,15 +1,35 @@
 package es.marcmauri.seccion_07_drawer_recycler_card
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import es.marcmauri.mylibrary.ToolbarActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : ToolbarActivity() {
+class MainActivity : ToolbarActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         toolbarToLoad(toolbar as Toolbar)
+
+        setNavDrawer()
+    }
+
+    private fun setNavDrawer() {
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, _toolbar,
+                R.string.open_drawer, R.string.close_drawer)
+        toggle.isDrawerIndicatorEnabled = true
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        //when(item.itemId)
+        return true
     }
 }
